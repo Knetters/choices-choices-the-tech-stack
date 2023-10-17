82cb094fcd101b9f98422eb6fc930f1976a8fee1
Tjips <thomas@scheepers.com>
2023-10-17 11:27:50 +0200

    Twee components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,48 @@
 import React from "react";
 
-const Header = () => {
+export default function Navbar() { 
 
-    let text = document.getElementById("text");
-    let layer1 = document.getElementById("layer1");
-    let layer2 = document.getElementById("layer2");
-    let layer3 = document.getElementById("layer3");
-    let layer4 = document.getElementById("layer4");
-    let layer5 = document.getElementById("layer5");
+    // let text = document.getElementById("text");
+    // let layer1 = document.getElementById("layer1");
+    // let layer2 = document.getElementById("layer2");
+    // let layer3 = document.getElementById("layer3");
+    // let layer4 = document.getElementById("layer4");
+    // let layer5 = document.getElementById("layer5");
 
-    window.addEventListener("scroll", () => {
-      let value = window.scrollY;
+    // window.addEventListener("scroll", () => {
+    //   let value = window.scrollY;
 
-      text.style.marginTop = value * 2.5 + "px";
-      layer2.style.left = value * 0.3 + "px";
-      layer1.style.left = value * -0.5 + "px";
-      layer4.style.left = value * 0.3 + "px";
-      layer5.style.left = value * 0.3 + "px";
-      layer3.style.left = value * -0.3 + "px";
-      layer2.style.top = value * 0.5 + "px";
-      layer1.style.top = value * 1 + "px";
-      layer3.style.top = value * 0.5 + "px";
-    });
+    //   text.style.marginTop = value * 2.5 + "px";
+    //   layer2.style.left = value * 0.3 + "px";
+    //   layer1.style.left = value * -0.5 + "px";
+    //   layer4.style.left = value * 0.3 + "px";
+    //   layer5.style.left = value * 0.3 + "px";
+    //   layer3.style.left = value * -0.3 + "px";
+    //   layer2.style.top = value * 0.5 + "px";
+    //   layer1.style.top = value * 1 + "px";
+    //   layer3.style.top = value * 0.5 + "px";
+    // });
 
-    let text2 = document.getElementById("text2");
-    let layer12 = document.getElementById("layer1-2");
-    let layer22 = document.getElementById("layer2-2");
-    let layer32 = document.getElementById("layer3-2");
-    let layer42 = document.getElementById("layer4-2");
-    let layer52 = document.getElementById("layer5-2");
+    // let text2 = document.getElementById("text2");
+    // let layer12 = document.getElementById("layer1-2");
+    // let layer22 = document.getElementById("layer2-2");
+    // let layer32 = document.getElementById("layer3-2");
+    // let layer42 = document.getElementById("layer4-2");
+    // let layer52 = document.getElementById("layer5-2");
 
-    window.addEventListener("scroll", () => {
-      let value = window.scrollY;
+    // window.addEventListener("scroll", () => {
+    //   let value = window.scrollY;
 
-      text2.style.marginTop = value * 2.5 + "px";
-      layer22.style.left = value * 0.3 + "px";
-      layer12.style.left = value * -0.5 + "px";
-      layer42.style.left = value * 0.3 + "px";
-      layer52.style.left = value * 0.3 + "px";
-      layer32.style.left = value * -0.3 + "px";
-      layer22.style.top = value * 0.5 + "px";
-      layer12.style.top = value * 1 + "px";
-      layer32.style.top = value * 0.5 + "px";
-    });
+    //   text2.style.marginTop = value * 2.5 + "px";
+    //   layer22.style.left = value * 0.3 + "px";
+    //   layer12.style.left = value * -0.5 + "px";
+    //   layer42.style.left = value * 0.3 + "px";
+    //   layer52.style.left = value * 0.3 + "px";
+    //   layer32.style.left = value * -0.3 + "px";
+    //   layer22.style.top = value * 0.5 + "px";
+    //   layer12.style.top = value * 1 + "px";
+    //   layer32.style.top = value * 0.5 + "px";
+    // });
 
   return (
     <div class="home-container">
@@ -333,5 +333,3 @@ const Header = () => {
     </div>
   );
 };
-
-export default Header;
