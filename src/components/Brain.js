4a59d9f173c862a1573eadfd9f3388b16942fb13
Tjips <thomas@scheepers.com>
2023-10-17 12:38:45 +0200

    Static website overgezet

---
+++ src/components/Brain.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Brain() {
+
+    return (
+        <section class="climate-container">
+            <div class="inner-section">
+            <div class="horizontal-flex-row">
+                <div class="text-container">
+                    <h2>b-RAIN</h2>
+                    <p class="bold-text">b-RAIN is het project rondom het koppelen van weerberichten aan wateropvang/gebruik in de tuin.</p>
+                    <p>Weersextremen in Nederland nemen steeds verder toe. Enerzijds komen in periodes van droogte, watertekorten vaker voor en anderzijds hebben we vaker te maken met extreme neerslag, met wateroverlast tot gevolg.</p>
+                    <p>B-rain wil bewoners positief betrokken houden bij het bufferen van regenwater en het groen houden van hun tuin. Door enkele gegevens in een app aan te geven, geeft deze app terug wanneer de regenwaterbuffer vol zit, wanneer deze geleegd moet worden (als er bijvoorbeeld een grote regenbui aan komt) en wanneer het lang niet regent (waardoor je zoveel mogelijk water in de regenwaterbuffer wil vasthouden).</p>
+                    <a href="#brain" class="button">Bekijk project</a>
+                </div>
+                <div class="image-container">
+                    <img src="img/bRAIN.png" alt="" />
+                </div>
+            </div>
+        </div>
+      </section>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from "react"
+
+import Layout from "../components/Layout";
+
 import Header from "../components/Header";
 import Over from "../components/Over";
 import Expertise from "../components/Expertise";
-
-import Layout from "../components/Layout";
 import Klimaatadaptatie from "../components/Klimaatadaptatie";
+import Waterkwaliteit from "../components/Waterkwaliteit";
+import Brain from "../components/Brain";
 
 export default function Home() {
   return (
@@ -14,6 +17,8 @@ export default function Home() {
         <Over />
         <Expertise />
         <Klimaatadaptatie />
+        <Waterkwaliteit />
+        <Brain />
       </div>
     </Layout>
   )
